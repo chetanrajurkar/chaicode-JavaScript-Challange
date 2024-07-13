c4832b9014bfe09c0f725daa7f96a0c4df48af9c
chetan007 <115970155+chetanrajurkar@users.noreply.github.com>
2024-07-13 16:12:50 +0530

    first challange is done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var a=4;
+console.log(typeof a);
+let b=4;
+console.log(typeof b);
+const c=false;
+console.log(typeof c);
 
+let num=345;
+console.log(typeof num);
+let string="hello world!!";
+console.log(typeof string);
+let obj={num:44,str:"hello"};
+console.log(typeof obj);
+let arr=[1,2,3,4,5,6,7,8,9];
+console.log(typeof arr);
+
+let arass=39;
+console.log(arass);
+arass="demo";
+console.log(arass);
